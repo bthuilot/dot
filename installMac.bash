b17357be19d87d29719a1dbed0b76457ef9e9c0c
Mill Creek Acomputer <bthuilot@gmail.com>
2017-02-13 16:47:11 -0500

    Push

--- installMac.bash
+++ installMac.bash
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+# Install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# Add cask
+brew tap caskroom/cask
 
 # Install zsh
 if [ -d "/bin/zsh"]
 then
   chsh -s /bin/zsh
 else
-  sudo apt-get install zsh
+  brew install zsh
   chsh -s /bin/zsh
 fi
 
@@ -14,10 +18,21 @@ fi
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sed -i -e 's/robbyrussell/agnoster/g' ~/.zshrc
 
-# Install Homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# The essentials
 
+# Command line stuff
 brew install git
-brew install lolcat
+brew install cowsay
+brew install fortune
+gem install lolcat
+
+# Applications
 brew cask install iterm2
-git clone 
+brew cask install atom
+brew cask install google-chrome
+brew cask install spotify
+brew cask install pibakery
+brw cask install etcher
+
+# Set up github folder
+mkdir ~/GitHub
