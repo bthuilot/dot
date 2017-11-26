b15db5ee41b78ab4a77af0100ae15759296c92c1
Bryce Thuilot <bthuilot@gmail.com>
2017-11-26 15:09:03 -0500

    Changes to Mac

--- Mac/installMac.sh
+++ Mac/installMac.sh
@@ -23,30 +23,23 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 # The essentials
 
 # Command line stuff
-brew install git
-brew install cowsay
-brew install fortune
-brew install gpg
+brew install git cowsay fortune gnupg chromedriver neovim rbenv npm
+
+npm install -g spaceship-zsh-theme
+
+cp files/zshrc ~/.zshrc
+
+source .zshrc
+
+rbenv install 2.4.2
+rbenv global 2.4.2
+rbenv shell 2.4.2
+
 sudo gem install lolcat
 sudo gem install sugarpaccione
 
 # Applications
-brew cask install iterm2
-brew cask install atom
-brew cask install google-chrome
-brew cask install spotify
-brew cask install pibakery
-brew cask install etcher
-brew cask install lastpass
-brew cask install sonos
-brew cask install the-unarchiver
-brew cask install flux
-brew cask install gpgtools
-brew cask install paragon-ntfs
-brew cask install utorrent
-brew cask install discord
-brew cask install steam
-brew cask install thunderbird
+ brew cask install iterm2 atom google-chrome spotify pibakery etcher lastpass sonos the-unarchiver gpgtools bittorrent discord steam thunderbird
 
 
 # Set up github folder
@@ -55,4 +48,3 @@ mkdir ~/GitHub
 echo 'Authy still needs to be installed from chrome'
 echo 'Files still need to be added from flash drive'
 echo 'Please add your ssh key to GitHub'
-echo 'Quotes also still need to be set up'
