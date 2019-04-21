e1a04895c4e6740712c9041aa4e84f562facfdf7
Bryce Thuilot <bthuilot@gmail.com>
2019-04-21 14:24:05 -0400

    Fixing bugs in backup-system.sh

--- backup-system.sh
+++ backup-system.sh
@@ -1,20 +1,36 @@
+#!/bin/bash
 
 ### Update backup file
 
+##############################
+### Unpack previous backup ###
+##############################
+
 # Decrypt Backup file
-gpg --decrypt ~/backup.tar.gz.gpg > ~/backup.tar.gz
-rm ~/backup.tar.gz.gpg
-# Expand tar.gz file
-tar -xzf ~/backup.tar.gz -C ~
+if [ -f $HOME/backup.tar.gz.gpg ];then
+  gpg --decrypt $HOME/backup.tar.gz.gpg > $HOME/backup.tar.gz
+  rm $HOME/backup.tar.gz.gpg
+  # Expand tar.gz file
+  tar -xzf $HOME/backup.tar.gz -C ~
+else 
+  mkdir -p $HOME/backup/
+  mkdir -p $HOME/backup/keys/
+fi
+
+###################
+### Backup Keys ###
+###################
 
 ## Move GPG Keys
+mkdir -p $HOME/backup/keys/gpg
 # Export public key
 gpg --armor --export bthuilot@gmail.com > ~/backup/keys/gpg/public.asc
 # Export secret key
 gpg --armor --export-secret-keys bthuilot@gmail.com > ~/backup/keys/gpg/secret.asc
 
-# Move SSH keys
-cp ~/.ssh/id* ~/backup/keys/ssh/
+## Move SSH keys
+mkdir -p $HOME/backup/keys/gpg
+cp ~/.ssh/id* $HOME/backup/keys/ssh/
 
 # Move to USB if attached
 if [ -d "/Volumes/THUILOT" ]; then
@@ -23,23 +39,15 @@ fi
 
 CURRENT_DIR=$(pwd)
 # Re-compress folder
-cd ~
+cd $HOME
 tar -czf backup.tar.gz backup
 # Remove backup
-rm -rf ~/backup
+rm -rf $HOME/backup
 # Encrypt tar.gz
-gpg -r bthuilot@gmail.com --encrypt ~/backup.tar.gz
+gpg -r bthuilot@gmail.com --encrypt $HOME/backup.tar.gz
 # Remove tar.gz
-rm -rf ~/backup.tar.gz
+rm -rf $HOME/backup.tar.gz
 
 ### Update Dot files
 cd $CURRENT_DIR
-# Update vim config
-cp ~/.config/nvim/init.vim vim/
-
-cp ~/.zshrc zsh/zshrc
-
-
-
-
 
