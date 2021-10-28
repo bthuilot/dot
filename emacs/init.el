755cc3bb412ee4d97b46dbff272426e4142279d6
Bryce Thuilot <bthuilot@gmail.com>
2021-10-28 12:41:57 -0400

    Update initel

--- emacs/init.el
+++ emacs/init.el
@@ -44,6 +44,8 @@
 
 ;; Set up package archives
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("elpa" . "https://elpa.gnu.org/packages/"))
+
 ;; Initialize package
 (package-initialize)
 
@@ -53,7 +55,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(highlight-parentheses company fira-code-mode flycheck sml-mode lsp-ui lsp-mode lsp-haskell projectile all-the-icons haskell-mode dashboard go-complete gruvbox-theme)))
+   '(markdown-preview-mode markdown-mode pandoc-mode highlight-parentheses company flycheck sml-mode lsp-ui lsp-mode lsp-haskell all-the-icons haskell-mode dashboard go-complete gruvbox-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -78,16 +80,16 @@
 ;;;;;;;;;;;;;;;;;;;;
 
 ;; Org mode related config
-(load "org_c")
+(load "orgc")
 
 ;; Dashboard related config
-(load "dashboard_c")
+(load "dash")
 
 ;; projectile related config
-(load "projectile_c")
+;;(load "projectile_c")
 
 ;; LSP relate config
-(load "lsp_c")
+(load "lsp")
 
 ;;;;;;;;;;;;;;;;;;;;
 ;; Styling config ;;
