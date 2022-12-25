8e6bdab23314c174fdf083e18cdfbd10a45a9697
Bryce Thuilot <bryce@thuilot.io>
2022-12-24 18:41:03 -0800

    initial script scaffolding

---
+++ macos/scripts/install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#
+# macOS install guide for https://github.com/bthuilot/dot
+# (C) Bryce Thuilot 2022 <bryce@thuilot.io>
+# License: GPL v3
+
+
+
+#######################################
+# Entry point of the install script
+# Globals:
+# Outputs:
+#   Writes status to STDOUT
+# Arguments:
+#   None
+#######################################
+function main() {
+    echo "TODO"
+}
+
+
+
+cat << EOF
+
+###################################
+     _       _    __ _ _           
+  __| | ___ | |_ / _(_) | ___  ___ 
+ / _\` |/ _ \| __| |_| | |/ _ \/ __|
+| (_| | (_) | |_|  _| | |  __/\__ \\
+ \__,_|\___/ \__|_| |_|_|\___||___/
+
+###################################                                   
+
+https://github.com/bthuilot/dot
+
+macOS install:
+      - 
+
+press any button to continue...
+EOF
+
+read # wait for enter
+main
