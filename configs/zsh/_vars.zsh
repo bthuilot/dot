327f844de04039070690e6b3f383ef303aad8b13
Bryce Thuilot <bryce@thuilot.io>
2025-10-18 07:32:51 -0700

    fix: OS specific zsh vars
    Signed-off-by: Bryce Thuilot <bryce@thuilot.io>

--- configs/zsh/_vars.zsh
+++ configs/zsh/_vars.zsh
@@ -25,26 +25,26 @@ export EDITOR='tem'
 export SSH_KEY_PATH="$HOME/.ssh/id_ed25519"
 export GOPATH="$HOME/go"
 export GOBIN="$GOPATH/bin"
-export XDG_DATA_HOME="$HOME/Library"
-export XDG_CONFIG_HOME="$HOME/Library/Preferences"
-export XDG_CACHE_HOME="$HOME/Library/Caches"
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nv
 
 # PATH exports
-export PATH="$HOME/bin:$HOME/.local/bin:/usr/local/bin:$PATH"
+export PATH="$HOME/.local/bin:/usr/local/bin:$PATH"
 export PATH="/usr/local/opt/openssl/bin:$PATH"
-export PATH="$PATH:$GOPATH/bin:$GOROOT/bin"
+export PATH="$PATH:$GOPATH/bin:$GOBIN"
 export PATH="/usr/local/sbin:$PATH"
-export PATH="$HOME/bin:/usr/local/bin:$PATH"
 export PATH="$HOME/.nimble/bin:$PATH"
 export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
 
 # OS specific
 if [[ $(uname) == "Darwin" ]]; then
-	export GOROOT="$(brew --prefix golang)/libexec"
-	export TERM=rxvt-256color
-	export PATH="/opt/homebrew/opt/llvm/bin:$PATH"
+    export XDG_DATA_HOME="$HOME/Library"
+    export XDG_CONFIG_HOME="$HOME/Library/Preferences"
+    export XDG_CACHE_HOME="$HOME/Library/Caches"
+    export GOROOT="$(brew --prefix golang)/libexec"
+    export TERM=rxvt-256color
+    export PATH="/opt/homebrew/opt/llvm/bin:$PATH"
 else
-	export GOROOT="/usr/local/go"
+    export GOROOT="/usr/local/go"
 fi
+
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nv
